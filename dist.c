cad7723df38bf70b8f9ddc160fe7ea454eedca7e
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-01-27 13:14:16 -0500

    asdas

---
+++ dist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 100
+#define l 3
+#define ini 1
+#define fin 20
+
+/*
+ *  Creates N random numbers between ini and fin according
+ *  to the distribution propto exp(-x/l)
+ */
+
+int main(){
+  int i;
+  for(i=0;i<N;i+=0){
+    //random
+  }
+
+
+  return 0;
+}
