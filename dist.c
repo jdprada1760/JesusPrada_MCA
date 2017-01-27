e74bde3776d4eb9af078fc8b10b828a5c3458603
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-01-27 13:25:37 -0500

    asdas

--- dist.c
+++ dist.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #define N 100
-#define l 3
+#define l 3.0
 #define ini 1
 #define fin 20
+#deifine filename "dist.data"
 
 /*
  *  Creates N random numbers between ini and fin according
@@ -12,11 +13,22 @@
  */
 
 int main(){
+  // Creates file to write data
+  FILE* fil = fopen(filename,"w");
   int i;
   for(i=0;i<N;i+=0){
-    //random
+    //random x
+    float x = ini+(fin-ini)*rand()/((float)RAND_MAX);
+    // Probability of obtaining x
+    float px = (1/l)*exp(-x/(l));
+    // Probability auxiliar
+    float py = rand()/((float)RAND_MAX);
+    // if py <= px it takes the number if not, it tries again
+    if( py <= px ){
+      fprintf(fil,"%f\n",x);
+      i++;
+    }
   }
-
-
+  fclose(fil);
   return 0;
 }
